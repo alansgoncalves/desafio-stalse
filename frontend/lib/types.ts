19afc575231cd82234908c4f9d8251bab01774a0
Alan Goncalves <vekio.soft@gmail.com>
2025-12-05 19:20:59 -0300

    feat: create type file for Ticket

--- frontend/lib/types.ts
+++ frontend/lib/types.ts
@@ -5,3 +5,13 @@ export interface Metrics {
   top_brands: Record<string, number>;
   top_products: Record<string, number>;
 }
+
+export interface Ticket {
+  id: number;
+  created_at: string;
+  customer_name: string;
+  channel: string;
+  subject: string;
+  status: 'open' | 'closed' | 'pending';
+  priority: 'low' | 'medium' | 'high';
+}
