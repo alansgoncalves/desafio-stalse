171f720a4fe25dfe3a0acf8fcf951112b0fce9da
Alan Goncalves <vekio.soft@gmail.com>
2025-12-06 20:04:47 -0300

    feat: add buton to redirect users

--- frontend/app/tickets/[id]/page.tsx
+++ frontend/app/tickets/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { fetchTicketDetails } from "@/lib/services";
 import { Ticket } from "@/lib/types";
 import { TicketDetailForm } from "@/components/TicketDetailForm";
+import Link from 'next/link'
 
 interface TicketPageProps {
   params: {
@@ -53,9 +54,17 @@ export default async function TicketPage({ params }: TicketPageProps) {
   }
 
   return (
-    <main className="container mx-auto p-8">
-      <h1 className="text-3xl font-bold mb-6 text-gray-500">Ticket #{ticket.id}: {ticket.subject}</h1>
+    <main className="container mx-auto p-4 sm:p-6 lg:p-8">
       
+      <div className="flex justify-between items-start mb-8 flex-col sm:flex-row sm:items-center">
+      <h1 className="text-3xl font-bold mb-6 text-gray-500">Ticket #{ticket.id}: {ticket.subject}</h1>
+
+      <Link href="/tickets" passHref>
+        <button className="bg-indigo-600 hover:bg-indigo-700 text-white font-medium py-2 px-4 rounded-lg shadow-md transition duration-150 cursor-pointer">
+          Voltar para lista de Tickets
+        </button>
+      </Link>
+      </div>
       <div className="bg-white p-6 rounded-lg shadow-xl">
         <div className="grid grid-cols-2 gap-4 mb-6 text-gray-700">
           <p><strong>Cliente:</strong> {ticket.customer_name}</p>
--- frontend/app/tickets/page.tsx
+++ frontend/app/tickets/page.tsx
@@ -2,6 +2,7 @@ import { fetchTickets } from "@/lib/services";
 import { TicketsTable } from "@/components/TicketsTable";
 import { Suspense } from "react";
 import { Ticket } from "@/lib/types";
+import Link from 'next/link'
 
 // Componente de página principal
 export default async function TicketsPage() {
@@ -17,19 +18,25 @@ export default async function TicketsPage() {
     }
 
     return (
-        <main className="container mx-auto p-4 sm:p-6 lg:p-8">
-            <h1 className="text-3xl font-bold mb-8 text-gray-500">Tickets Mini Inbox</h1>
-            
-            {error ? (
-                <div className="text-center p-8 bg-red-100 text-red-700 rounded-lg">
-                    <p>{error}</p>
-                </div>
-            ) : (
-                <Suspense fallback={<div className="text-center">Preparando a tabela...</div>}>
-                    {/* Passa a lista inicial para o componente cliente */}
-                    <TicketsTable initialTickets={initialTickets} />
-                </Suspense>
-            )}
-        </main>
+      <main className="container mx-auto p-4 sm:p-6 lg:p-8">
+        <div className="flex justify-between items-start mb-8 flex-col sm:flex-row sm:items-center">
+          <h1 className="text-3xl font-bold mb-8 text-gray-500">Tickets Mini Inbox</h1>
+            <Link href="/dashboard" passHref>
+              <button className="bg-indigo-600 hover:bg-indigo-700 text-white font-medium py-2 px-4 rounded-lg shadow-md transition duration-150 cursor-pointer">
+                Voltar Dashboard
+              </button>
+            </Link>
+        </div>
+          {error ? (
+            <div className="text-center p-8 bg-red-100 text-red-700 rounded-lg">
+                <p>{error}</p>
+            </div>
+          ) : (
+            <Suspense fallback={<div className="text-center">Preparando a tabela...</div>}>
+                {/* Passa a lista inicial para o componente cliente */}
+              <TicketsTable initialTickets={initialTickets} />
+            </Suspense>
+          )}
+      </main>
     );
 }
